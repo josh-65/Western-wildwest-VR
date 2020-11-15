d29dc6d2b826499f349fe88e26e0839713b7dfd9
Josh-65 <joshuauk2004@gmail.com>
2020-11-15 16:16:35 +0000

    Updated menus

---
+++ Assets/_Scripts/timeCycle.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class timeCycle : MonoBehaviour
+{
+    void Update()
+    {
+        transform.RotateAround(Vector3.zero,Vector3.right, 0.01668f*Time.deltaTime);
+        transform.LookAt(Vector3.zero);
+    }
+}
